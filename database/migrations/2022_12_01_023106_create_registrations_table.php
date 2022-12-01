38a8be90a2b79f26bb72c6e3137972cef3fdaf5f
jcardenasv <jcardenasv@unal.edu.co>
2022-11-30 23:00:31 -0500

    feat: tables registrations, attendance_records and class_attendances

---
+++ database/migrations/2022_12_01_023106_create_registrations_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_course');
+            $table->unsignedBigInteger('id_student');
+            $table->foreign('id_course')
+                ->references('id')
+                ->on('courses')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('id_student')
+                ->references('id')
+                ->on('students')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrations');
+    }
+};
